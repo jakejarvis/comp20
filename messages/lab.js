62834217b461fb12ffa60c818b8c258364b6bdfc
Jake Jarvis <jakejarvis@gmail.com>
2015-03-10 09:24:08 -0400

    data.json working with rough CSS

---
+++ messages/lab.js
@@ -0,0 +1,23 @@
+var xhr;
+
+function parse() {
+    xhr = new XMLHttpRequest();
+    xhr.open("get", "data.json", true);
+    xhr.onreadystatechange = ready;
+    xhr.send(null);
+}
+
+function ready() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        data = JSON.parse(xhr.responseText);
+
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i]["content"]);
+
+            document.getElementById("messages").innerHTML += '<p><span class="message">' + data[i]["content"] + '<span class="username">' + data[i]["username"] + '</span></span></p>';
+        }
+
+    } else if (xhr.readyState == 4 && xhr.status == 500) {
+        alert("Sorry, something went wrong!");
+    }
+}
